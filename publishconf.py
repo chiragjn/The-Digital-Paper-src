3eba9165ff4862bff49b191ba7360ba109d07ad8
chiragjn <jain.chirag925@gmail.com>
2017-03-30 14:41:34 +0530

    Fix Minxed Content Erros by serving over https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://chiragjn.github.io/blog'
+SITEURL = 'https://chiragjn.github.io/blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
